1df4201e8045459c3ba5484bacb69d39477cf022
Harish <hkantheti@yodlee.com>
2017-08-03 12:31:10 +0530

    Made to-do item appear in the list but numbering is a cheat for now

--- lists/tests.py
+++ lists/tests.py
@@ -1,13 +1,13 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from django.template.loader import render_to_string
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_post_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
