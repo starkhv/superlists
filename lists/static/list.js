2f39e6229280ededd9d73e0b3f0241d6af312b99
Harish <kharishvarma@gmail.com>
2017-09-10 16:03:00 +0530

    errors are hidden if input box is focused

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,8 @@
 window.Superlists = {};
 window.Superlists.initialize = function () {
+	$("input[name='text']").on("focus", function() {
+		$(".has-error").hide();
+	});
 	$("input[name='text']").on("keypress", function() {
 		$(".has-error").hide();
 	});
