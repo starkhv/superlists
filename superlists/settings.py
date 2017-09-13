d333e2c0b9cee6db404d847a817e923aa0ff6806
Harish <kharishvarma@gmail.com>
2017-09-13 09:12:37 +0530

    spiked in custom passwordless auth backend

--- superlists/settings.py
+++ superlists/settings.py
@@ -39,7 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
+    'django_extensions',
 ]
+
+# start - added by KHV
+AUTH_USER_MODEL = 'accounts.ListUser'
+AUTHENTICATION_BACKENDS = [
+        'accounts.authentication.PasswordlessAuthenticationBackend',
+        ]
+# end - added by KHV
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -121,3 +130,26 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ilovesarkarjobs@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                },
+            },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+                },
+            },
+        'root': {'level': 'INFO'},
+        }
