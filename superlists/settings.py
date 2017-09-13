2e648b41861fcf6ef951b4863eb04db2befe3aa7
Harish <kharishvarma@gmail.com>
2017-09-13 09:20:19 +0530

    Changed HOST USER

--- superlists/settings.py
+++ superlists/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ilovesarkarjobs@gmail.com'
+EMAIL_HOST_USER = 'EMAIL_HOST_USER HERE'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
