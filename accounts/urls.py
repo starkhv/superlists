9620d1314d69f91d3221a7395748ce82e17f8e16
Harish <kharishvarma@gmail.com>
2017-10-10 08:59:17 +0530

    end of chapter 19 Using Mocks

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,13 @@
+from django.contrib.auth.views import logout
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-        url(r'send_login_email',
+        url(r'^send_login_email$',
             views.send_login_email,
             name=r'send_login_email'
             ),
-        url(r'login',
-            views.login,
-            name=r'login'
-            )
+        url(r'^login$', views.login, name=r'login'),
+        url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
         ]
